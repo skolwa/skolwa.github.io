7925331b8fc499080e219424cf47c752675ce60b
Sthabile Kolwa <senangakuti@galaxy-finder.wlan.dur.ac.uk>
2023-11-13 22:25:17 +0000

    navigation update

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', event => {
     // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
+        new bootstrap.Pagination(document.body, {
             target: '#sideNav',
             offset: 74,
         });
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,28 +2,28 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
+// window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            offset: 74,
-        });
-    };
+//     // Activate Bootstrap scrollspy on the main nav element
+//     const sideNav = document.body.querySelector('#sideNav');
+//     if (sideNav) {
+//         new bootstrap.Pagination(document.body, {
+//             target: '#sideNav',
+//             offset: 74,
+//         });
+//     };
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
+//     // Collapse responsive navbar when toggler is visible
+//     const navbarToggler = document.body.querySelector('.navbar-toggler');
+//     const responsiveNavItems = [].slice.call(
+//         document.querySelectorAll('#navbarResponsive .nav-link')
+//     );
+//     responsiveNavItems.map(function (responsiveNavItem) {
+//         responsiveNavItem.addEventListener('click', () => {
+//             if (window.getComputedStyle(navbarToggler).display !== 'none') {
+//                 navbarToggler.click();
+//             }
+//         });
+//     });
 
-});
+// });
